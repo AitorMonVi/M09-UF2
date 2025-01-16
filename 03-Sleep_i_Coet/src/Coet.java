68403ffe26bb04ec4f3f809e27351b28761cdf11
AitorMonVi <2023_aitor.montejo@iticbcn.cat>
2025-01-16 16:47:09 +0100

    añadido metodo passaAPotencia()

--- 03-Sleep_i_Coet/src/Coet.java
+++ 03-Sleep_i_Coet/src/Coet.java
@@ -5,15 +5,20 @@ public class Coet {
     private static Motor[] motores = new Motor[4];
     private static int potencia;
 
+    public static void passaAPotencia(Motor motor, int p) throws Exception {
+            if(p>10 || p<0) throw new Exception("Potencia no valida");
+            else motor.setPotencia(p); 
+    }
+
     private static void arranca() throws Exception {
         System.out.println("Passant a potència " + potencia);
         for(int i = 0; i < motores.length; i++) {
-            if (motores[i] != null && motores[i].isAlive()) motores[i].setPotencia(potencia);
+            if (motores[i] != null && motores[i].isAlive()) passaAPotencia(motores[i], potencia);
             else {
                 int actual = 0;
                 if(motores[i]!=null) actual = motores[i].getPotencia();
                 motores[i] = new Motor("Motor "+i);
-                motores[i].setPotencia(potencia);
+                passaAPotencia(motores[i], potencia);
                 motores[i].setPotenciaActual(actual);
                 motores[i].start();
             }
